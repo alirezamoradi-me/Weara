1eb83128369b148cc74dee5ff0d2e99279adc624
Alireza Moradi <me.alirezamoradi@gmail.com>
2026-02-12 18:29:47 +0330

    configure CORS and security headers
    Co-authored-by: amir01826 <2cbe567335@webxio.pro>

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,16 @@ import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(helmet());
+  app.enableCors({
+    origin: [
+      "http://localhost:3000"
+    ],
+    methods: "GET, POST, PUT, PATCH, DELETE",
+    credentials: true,
+    allowedHeaders: "Content-Type, Authorization"
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
